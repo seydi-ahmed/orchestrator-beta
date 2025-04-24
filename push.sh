7ff4559f0d304d2d2f6ded7d5428c58e3e0bc786
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-04-24 15:00:38 +0000

    ajout de manifests et scripts

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "ajout de vagrantfile et de orchestrator.sh"
+git commit -m "ajout de manifests et scripts"
 git push --force
