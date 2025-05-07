77f07f44add84dba05638eb69f3e20ade13856d1
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-05-07 19:56:55 +0000

    ajout de configmap

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "remplacer les hosts par les variables d'env"
+git commit -m "ajout de configmap"
 git push --force
