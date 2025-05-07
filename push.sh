2e90050c4217776e130da6f58741a077d6a1b463
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-05-07 08:45:54 +0000

    Inventory-db, billing-db et api-gatewa sont en running

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "créer le chemin directement dane le vagrantfile"
+git commit -m "Inventory-db, billing-db et api-gatewa sont en running"
 git push --force
