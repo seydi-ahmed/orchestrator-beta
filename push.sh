081c4f6f45250c143c6ae1928922fc472878a699
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-05-07 15:35:21 +0000

    étapes à suivre aprés le démarrage des VM

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "suppression de initcontainer"
+git commit -m "étapes à suivre aprés le démarrage des VM"
 git push --force
